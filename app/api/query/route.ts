64c3410a1abf28a97de5e490038db8ad6d9cf054
Pascal Dimassimo <pascal@pascaldimassimo.com>
2023-06-20 09:49:15 -0400

    Used public endpoint, added a prompt, and used basic params to have an answer ASAP

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -9,15 +9,15 @@ export async function POST(
 
   try {
 
-    const app = await client("http://10.100.11.139:7860/");
+    const app = await client("https://914279084c0d3949fa.gradio.live/");
     const result = await app.predict("/predict", [
-      query, // string  in 'Instruction' Textbox component		
-      "", // string  in 'Input' Textbox component		
+      "You're Help Scout support agent, you're always answering questions politely, comprehensively and concisely. If you're not sure about the answer please ask for more details.", // string  in 'Instruction' Textbox component
+      query, // string  in 'Input' Textbox component
       0.1, // number (numeric value between 0 and 1) in 'Temperature' Slider component		
       0.75, // number (numeric value between 0 and 1) in 'Top p' Slider component		
-      40, // number (numeric value between 0 and 100) in 'Top k' Slider component		
-      4, // number (numeric value between 1 and 4) in 'Beams' Slider component		
-      128, // number (numeric value between 1 and 2000) in 'Max tokens' Slider component		
+      10, // number (numeric value between 0 and 100) in 'Top k' Slider component
+      1, // number (numeric value between 1 and 4) in 'Beams' Slider component
+      100, // number (numeric value between 1 and 2000) in 'Max tokens' Slider component
       false, // boolean  in 'Stream output' Checkbox component
     ]);
 
